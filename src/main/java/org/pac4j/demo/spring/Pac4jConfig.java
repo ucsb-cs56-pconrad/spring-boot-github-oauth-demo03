beb16a012107487e7f8ba6ffd48ae9f7bc6ba804
Phill Conrad <pconrad@cs.ucsb.edu>
2018-10-18 12:20:16 -0700

    Github working, facebook is not

--- src/main/java/org/pac4j/demo/spring/Pac4jConfig.java
+++ src/main/java/org/pac4j/demo/spring/Pac4jConfig.java
@@ -20,19 +20,50 @@ public class Pac4jConfig {
 
     @Value("${salt}")
     private String salt;
+    
+    @Value("${baseUrl}")
+    private String baseUrl;
+
+    @Value("${github_client_id}")
+    private String github_client_id;
+
+    @Value("${github_client_secret}")
+    private String github_client_secret;
+
+    @Value("${facebook_client_id}")
+    private String facebook_client_id;
+
+    @Value("${facebook_client_secret}")
+    private String facebook_client_secret;
 
     @Bean
     public Config config() {
 
-        final FacebookClient facebookClient = new FacebookClient("145278422258960", "be21409ba8f39b5dae2a7de525484da8");
-        final GitHubClient ghClient = new GitHubClient("CoxUiYwQOSFDReZYdjigBA",
-                "2kAzunH5Btc4gRSaMr7D7MkyoJ5u1VzbOOzE8rBofs");
+        final FacebookClient facebookClient =
+	    new FacebookClient(facebook_client_id, facebook_client_secret);
+
+	// Facebook Scopes:
+	// https://stackoverflow.com/questions/30094382/facebook-invalid-scope-error
+	// https://developers.facebook.com/docs/facebook-login/permissions/
+	// To avoid triggering app review, use "default" or "email" only.
+	// The default value in the FacebookClient class is not "default",
+	// unfortunately, and includes invalid scopes.
+	
+	facebookClient.setScope("default"); // (or "email")
+	
+        final GitHubClient ghClient =
+	    new GitHubClient(github_client_id, github_client_secret);
 
         final Clients clients =
-	    new Clients("http://localhost:8080/callback",
+	    new Clients(baseUrl + "/callback",
 			facebookClient,
 			ghClient);
 
+	// In that case, the callback URL of the FacebookClient is
+	// baseUrl + "/callback?client_name=FacebookClient
+	// and the callback URL of the GitHubClient is
+	// baseUrl + "/callback?client_name=GitHubClient.
+	
         final Config config = new Config(clients);
         config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("ROLE_ADMIN"));
         config.addAuthorizer("custom", new CustomAuthorizer());
